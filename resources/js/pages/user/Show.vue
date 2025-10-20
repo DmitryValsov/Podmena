075cdc6da2a771492d232c2d7ec37424dcb1e18d
Dmitrii <dmitrii@Air-Dmitrii.lan>
2025-10-20 21:55:01 +0500

    first commit

---
+++ resources/js/pages/user/Show.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+
+import { Head, usePage } from '@inertiajs/vue3';
+
+//defineProps<Props>();
+
+const page = usePage();
+const user = page.props.auth.user;
+
+</script>
+
+<template>
+
+        <Head title="Welcome" />
+        <h1>Welcome</h1>
+
+        <p>Hello {{ user.name }}, welcome to your first Inertia app!</p>
+
+</template>
